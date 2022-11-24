0942a69226098f9f0272e294110c49e0886923fe
farid <faridhimeur@outlook.fr>
2022-11-24 15:19:48 +0100

    create header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="logosearchbar">
+        <p>HELLO</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
