36e2c61742d9a86fd584e943e0d37015455f551b
wild-valentin <marlois.valentin@gmail.com>
2022-11-25 10:50:52 +0100

    get data on the leaflet

--- frontend/src/data/api.js
+++ frontend/src/data/api.js
@@ -1,6 +1,17 @@
 export default function getData(lat, lgn) {
+  const dataAPI = fetch(
+    `https://api.opentripmap.com/0.1/en/places/radius?radius=50000&lon=${lgn}&lat=${lat}&kinds=museums&format=json&apikey=5ae2e3f221c38a28845f05b672f12987087d7275fb9e12b59b197eaf`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    });
+  return dataAPI;
+}
+
+export function getdetails(xid) {
   fetch(
-    `https://api.opentripmap.com/0.1/en/places/radius?radius=50000&lon=${lgn}&lat=${lat}&kinds=cultural&format=json&apikey=5ae2e3f221c38a28845f05b672f12987087d7275fb9e12b59b197eaf`
+    `https://api.opentripmap.com/0.1/en/places/xid/${xid}?apikey=5ae2e3f221c38a28845f05b672f12987087d7275fb9e12b59b197eaf`
   )
     .then((res) => res.json())
     .then((data) => console.log(data));
